26574ca3b65371b0271a08538319f3d4ee45405f
skyfy16 <colin.viatte@gmail.com>
2023-01-02 12:20:08 +0100

    update

--- src/main/java/ch/skyfy/entitycrammingdropxp/EntityCrammingDropXP.java
+++ src/main/java/ch/skyfy/entitycrammingdropxp/EntityCrammingDropXP.java
@@ -1,19 +1,11 @@
 package ch.skyfy.entitycrammingdropxp;
 
 import net.fabricmc.api.ModInitializer;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @SuppressWarnings("unused")
 public class EntityCrammingDropXP implements ModInitializer {
 
-    public static final String MODID = "entitycrammingdropxp";
-
-    public static final Logger LOGGER = LogManager.getLogger("EnderBackpack");
-
     @Override
-    public void onInitialize() {
-
-    }
+    public void onInitialize() {}
 
 }
