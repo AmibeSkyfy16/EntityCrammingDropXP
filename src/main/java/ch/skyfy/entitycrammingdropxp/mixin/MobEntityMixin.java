5780dd3750b0f6365e43f97a3529421a5d08dbef
skyfy16 <colin.viatte@gmail.com>
2023-03-19 18:44:42 +0100

    1.19.4

--- src/main/java/ch/skyfy/entitycrammingdropxp/mixin/MobEntityMixin.java
+++ src/main/java/ch/skyfy/entitycrammingdropxp/mixin/MobEntityMixin.java
@@ -2,18 +2,24 @@ package ch.skyfy.entitycrammingdropxp.mixin;
 
 import net.minecraft.entity.ExperienceOrbEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageSources;
+import net.minecraft.entity.damage.DamageType;
+import net.minecraft.entity.damage.DamageTypes;
 import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.registry.Registries;
 import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MobEntity.class)
-public abstract class MobEntityMixin extends LivingEntityMixin{
-    @Shadow protected int experiencePoints;
+public abstract class MobEntityMixin extends LivingEntityMixin {
+    @Shadow
+    protected int experiencePoints;
+
     @Override
     public void apply(DamageSource damageSource, CallbackInfo info) {
-        if (damageSource.equals(DamageSource.CRAMMING)) {
+        if (damageSource.getTypeRegistryEntry().matchesId(DamageTypes.CRAMMING.getValue())) {
             var mobEntity = (MobEntity) (Object) this;
             ExperienceOrbEntity.spawn((ServerWorld) mobEntity.world, mobEntity.getPos(), experiencePoints);
         }
